4210c7e1c355f9f17dfd26ca3e181c0f56207e73
alessandrodias83 <158523798+alessandrodias83@users.noreply.github.com>
2024-06-27 17:53:00 -0300

    up

---
+++ aula16/index.js
@@ -0,0 +1,17 @@
+//    Elementos   0            1          2    3   4
+const alunos = ['Alessandro', 'Ligia', 'Dias', 1, true];
+
+//delete alunos[1]; - deleta um elemento e a posição fica vazia
+
+//const removido = alunos.shift(); - remove o primeiro elemento
+
+//alunos.pop(); retira o último elemento
+
+//alunos.unshift('Luiza'); - acrescenta no inicio 
+
+//alunos[2] = 'Eduardo'; - acrescenta um item no fim
+
+//alunos.push('Luiza'); - acrescenta no fim
+
+
+console.log(alunos);
