420082e9696884ad3f17c60d9402ffbc18ebe686
alessandrodias83 <158523798+alessandrodias83@users.noreply.github.com>
2024-06-27 18:26:00 -0300

    up

---
+++ aula17/index.js
@@ -0,0 +1,28 @@
+/*function saudacao(nome) {
+    return `Boa noite ${nome}!`;
+}
+
+const variavel = saudacao('Alessandro');
+console.log(variavel)
+
+function soma(x, y) { // primeiro tipo de criar função
+    const resultado = x + y;
+    return resultado; // apos return não roda mais nada
+}
+
+console.log(soma(2, 2));
+
+console.log(soma(5, 20));
+
+console.log(soma(6, 12));
+
+const raiz = function (n) { // segundo tipo de criar função
+    return n ** 0.5;
+};
+
+console.log(raiz(9));*/
+
+const raiz = (n) => n ** 0.5; //terceiro tipo de criar função
+
+console.log(raiz(9));
+
