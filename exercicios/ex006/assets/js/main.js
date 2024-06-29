a0ac70fe44e49e1bb8d73d979692e5b9c36f1b00
alessandrodias83 <158523798+alessandrodias83@users.noreply.github.com>
2024-06-29 10:17:48 -0300

    up

---
+++ exercicios/ex006/assets/js/main.js
@@ -0,0 +1,17 @@
+// capturar evento de submit do formulário
+const form = document.querySelector('#formulario');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log('Evento prevenido');
+    setResultado('Olá Mundo!')
+});
+
+function setResultado (msg) {
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+    const p = document.createElement('p');
+    p.classList.add('paragrafo-resultado');
+    p.innerHTML = 'teste';
+    resultado.appendChild(p);
+}
